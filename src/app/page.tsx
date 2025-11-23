f95e88320551ea392d7dc66c751013b58f97c6ee
Mihir <31316452+mihirterna@users.noreply.github.com>
2025-11-23 22:08:22 +0530

    vercel speedinsight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { GameProvider } from '@/context/GameContext';
 import GameBoard from '@/components/GameBoard';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <GameProvider>
         <GameBoard />
       </GameProvider>
+      <SpeedInsights />
     </main>
   );
 }
